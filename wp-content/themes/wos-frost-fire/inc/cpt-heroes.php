99ad666a82ef3b0f06efcbd99b676364ce106a35
ryohryp <ryohryp@gmail.com>
2026-02-11 17:12:27 +0900

    feat: ヒーローカスタム投稿タイプと関連するタクソノミー、メタボックスを実装しました

--- wp-content/themes/wos-frost-fire/inc/cpt-heroes.php
+++ wp-content/themes/wos-frost-fire/inc/cpt-heroes.php
@@ -13,9 +13,6 @@ class WoS_Hero_CPT {
         add_action( 'add_meta_boxes', [ $this, 'register_meta_boxes' ] );
         add_action( 'save_post', [ $this, 'save_meta_box_data' ] );
     }
-    add_action( 'add_meta_boxes', [ $this, 'register_meta_boxes' ] );
-        add_action( 'save_post', [ $this, 'save_meta_box_data' ] );
-    }
 
     /**
      * Register Custom Post Type.
